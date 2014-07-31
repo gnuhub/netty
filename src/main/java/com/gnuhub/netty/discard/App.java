481e2ab281d1db8de0db69710636fae61d7083b3
gnuhub <gnuhub@gmail.com>
2014-07-31 11:03:05 +0800

    使用构造函数向对象传递 see #2

--- src/main/java/com/gnuhub/netty/discard/App.java
+++ src/main/java/com/gnuhub/netty/discard/App.java
@@ -6,6 +6,11 @@ package com.gnuhub.netty.discard;
  */
 public class App 
 {
+    private int port;
+
+    public App(int port){
+        this.port = port;
+    }
     public static void main( String[] args )
     {
         // 实现可以从命令行接收参数指定服务器要绑定的端口
@@ -16,5 +21,7 @@ public class App
             port = 8077;
         }
         System.out.println(port);
+        // 使用构造函数向对象传递参数
+        System.out.println(new App(port).port);
     }
 }
