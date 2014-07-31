3a5bf944bedef98921c2ed380e57f7c9f04cbd0e
gnuhub <gnuhub@gmail.com>
2014-07-31 10:55:59 +0800

    完成可以从命令行接收参数

--- src/main/java/com/gnuhub/netty/discard/App.java
+++ src/main/java/com/gnuhub/netty/discard/App.java
@@ -8,6 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // 实现可以从命令行接收参数指定服务器要绑定的端口
+        int port;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        } else {
+            port = 8077;
+        }
+        System.out.println(port);
     }
 }
