a845d963bf156114571b2342a1f062f5d0e3bc02
gnuhub <gnuhub@gmail.com>
2014-07-31 10:27:09 +0800

    mvnapp.new netty discard

---
+++ src/main/java/com/gnuhub/netty/discard/App.java
@@ -0,0 +1,13 @@
+package com.gnuhub.netty.discard;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
